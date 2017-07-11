b2508a729eaf09423ae926dad3656656c5f4ed21
timetam1 <tanuma321bre@gmail.com>
2017-07-11 12:22:32 +0900

    Update and rename .gitignore to staff.php

---
+++ .gitignore/staff.php
@@ -0,0 +1,14 @@
+<form action = "staff_check.php" method = "post">
+<dl>
+<dt>名前</dt>
+<dd>
+	<input type = "text" name = "name" > 
+</dd>
+<dt>パスワード</dt>
+<dd>
+	<input type = "password" name  ="pass" >
+</dd>
+</dl>
+<input type = "submit" value = "送信する"　/>
+	
+</form>
